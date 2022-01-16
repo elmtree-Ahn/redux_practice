66c24a910c17fb9289f088fb56c13fafb3efac8d
elmtree-Ahn <clfclf3138@gmail.com>
2022-01-16 20:50:52 +0900

    Redux로 Todo 관리하기

--- src/index.js
+++ src/index.js
@@ -5,17 +5,71 @@ const form = document.querySelector("form");
 const input = document.querySelector("input");
 const ul = document.querySelector("ul");
 
-const createToDo = todo => {
-  const li = document.createElement("li");
-  li.innerText = todo;
-  ul.appendChild(li);
+const ADD_TODO = "ADD_TODO";
+const DELETE_TODO = "DELETE_TODO";
+
+const addToDo = (text) => {
+  return {
+    type: ADD_TODO, text
+  }
+}
+
+const deleteToDo = (id) => {
+  return {
+    type: DELETE_TODO, id
+  }
+}
+
+const reducer = (state = [], action) => {
+  console.log(action)
+  switch(action.type) {
+    case ADD_TODO:
+      return [ { text: action.text, id: Date.now()}, ...state];
+    case DELETE_TODO:
+      return state.filter(toDo => toDo.id !== parseInt(action.id));
+    default:
+      return state; 
+  }
+}; 
+
+const store = createStore(reducer)
+
+store.subscribe(() => console.log(store.getState()));
+
+const dispatchAddToDo = (text) => {
+  store.dispatch(addToDo(text))
+}
+
+const dispatchDeleteToDo = (e) => {
+  const id = e.target.parentNode.id;
+  store.dispatch(deleteToDo(id))
+}
+
+const paintTodos = () => {
+  const toDos = store.getState();
+  ul.innerHTML = "";
+  toDos.forEach(todo => {
+    const li = document.createElement("li");
+    const btn = document.createElement("button");
+    btn.innerText = "DEL";
+    btn.addEventListener("click", dispatchDeleteToDo)
+    li.id = todo.id;
+    li.innerText = todo.text;
+    li.appendChild(btn);
+    ul.appendChild(li);
+  });
 };
+
+store.subscribe(paintTodos);
+
+
 
 const onSubmit = e => {
   e.preventDefault();
   const todo = input.value;
   input.value = "";
-  createToDo(todo)
+  dispatchAddToDo(todo);
+  
 };
 
 form.addEventListener("submit", onSubmit);
